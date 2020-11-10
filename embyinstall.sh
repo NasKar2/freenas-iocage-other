9c717cab4639d9078d57179075dfb3c411252331
root <root@truenas.local>
2020-11-09 19:08:21 -0500

    emby defaults

--- embyinstall.sh
+++ embyinstall.sh
@@ -12,7 +12,7 @@ fi
 JAIL_IP=""
 DEFAULT_GW_IP=""
 INTERFACE=""
-VNET="off"
+VNET=""
 POOL_PATH=""
 APPS_PATH=""
 EMBY_DATA=""
@@ -27,7 +27,7 @@ CONFIGS_PATH=$SCRIPTPATH/configs
 DB_ROOT_PASSWORD=$(openssl rand -base64 16)
 DB_PASSWORD=$(openssl rand -base64 16)
 ADMIN_PASSWORD=$(openssl rand -base64 12)
-RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g" | sed "s/-p[0-9]*//")
+RELEASE=$(freebsd-version | cut -d - -f -1)"-RELEASE"
 
 # Check for emby-config and set configuration
 if ! [ -e $SCRIPTPATH/emby-config ]; then
@@ -45,32 +45,39 @@ if [ -z $DEFAULT_GW_IP ]; then
   exit 1
 fi
 if [ -z $INTERFACE ]; then
-  echo 'Configuration error: INTERFACE must be set'
-  exit 1
+  INTERFACE="vnet0"
+  echo "INTERFACE defaulting to 'vnet0'"
 fi
-if [ -z $POOL_PATH ]; then
-  echo 'Configuration error: POOL_PATH must be set'
-  exit 1
+if [ -z $VNET ]; then
+  VNET="on"
+  echo "VNET defaulting to 'on'"
 fi
 
+if [ -z $POOL_PATH ]; then
+  POOL_PATH="/mnt/$(iocage get -p)"
+  echo "POOL_PATH defaulting to "$POOL_PATH
+fi
 if [ -z $APPS_PATH ]; then
-  echo 'Configuration error: APPS_PATH must be set'
-  exit 1
+  APPS_PATH="apps"
+  echo "APPS_PATH defaulting to 'apps'"
 fi
 
 if [ -z $JAIL_NAME ]; then
-  echo 'Configuration error: JAIL_NAME must be set'
-  exit 1
+  JAIL_NAME="emby"
+  echo "JAIL_NAME defaulting to 'emby'"
 fi
-
 if [ -z $EMBY_DATA ]; then
-  echo 'Configuration error: EMBY_DATA must be set'
-  exit 1
+  EMBY_DATA="emby"
+  echo "EMBY_DATA defaulting to 'emby'"
 fi
-
 if [ -z $MEDIA_LOCATION ]; then
-  echo 'Configuration error: MEDIA_LOCATION must be set'
-  exit 1
+  MEDIA_LOCATION="media"
+  echo "MEDIA_LOCATION defaulting to 'media'"
+fi
+TORRENTS_LOCATION=""
+if [ -z $TORRENTS_LOCATION ]; then
+  TORRENTS_LOCATION="toreents"
+  echo "TORRENTS_LOCATION defaulting to 'torrents'"
 fi
 
 #
